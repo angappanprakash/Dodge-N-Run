05421b63195f94bdcb897175cc63b0f88efebe13
angappanprakash <37359602+angappanprakash@users.noreply.github.com>
2018-03-27 12:44:59 +0530

    Player Controller - WIP
    Added player sprite sheets and set up basic animation states.

---
+++ Assets/Scripts/GameElements/PlayerComponents/PlayerData.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerData
+{
+#region Variables
+	private float               mSFXVolume;
+	private float               mMusicVolume;
+	private bool                mFirstTimeLaunch;
+
+	private const float         DEFAULT_SFX_VOLUME = 1.0f;
+	private const float         DEFAULT_MUSIC_VOLUME = 1.0f;
+
+	private const string        SFX_VOLUME_KEY = "SFXVolume";
+	private const string        MUSIC_VOLUME_KEY = "MusicVolume";
+
+	private const string        FIRST_TIME_LAUNCH = "FirstTimeLauched";
+	private static PlayerData   mInstance;
+#endregion
+
+#region Properties
+	public float pSFXVolume
+	{
+		get {   return mSFXVolume;  }
+	}
+
+	public float pMusicVolume
+	{
+		get { return mMusicVolume;  }
+	}
+	public bool pFirstTimeLaunch
+	{
+		get { return mFirstTimeLaunch; }
+	}
+
+	public static PlayerData pInstance
+	{
+		get { return mInstance; }
+	}
+#endregion
+
+#region Class specific functions
+	public static void Init()
+	{
+		mInstance = new PlayerData();
+	}
+
+	private PlayerData()
+	{
+		if (PlayerPrefs.HasKey(SFX_VOLUME_KEY))
+		{
+			mSFXVolume = PlayerPrefs.GetFloat(SFX_VOLUME_KEY);
+		}
+		else
+		{
+			UpdateSFXVolume(DEFAULT_SFX_VOLUME);
+		}
+
+		if (PlayerPrefs.HasKey(MUSIC_VOLUME_KEY))
+		{
+			mMusicVolume = PlayerPrefs.GetFloat(MUSIC_VOLUME_KEY);
+		}
+		else
+		{
+			UpdateMusicVolume(DEFAULT_MUSIC_VOLUME);
+		}
+
+		if (PlayerPrefs.HasKey(FIRST_TIME_LAUNCH))
+		{
+			mFirstTimeLaunch = (PlayerPrefs.GetInt(FIRST_TIME_LAUNCH) == 1) ? true : false ;
+		}
+		else
+		{
+			SetFirstTimeLauched(false);
+		}
+	}
+
+	public void UpdateSFXVolume(float sfxVolume)
+	{
+		mSFXVolume = sfxVolume;
+//		if(AudioManager.pInstance)
+//			AudioManager.pInstance.UpdateSFXVolume(mSFXVolume);
+		PlayerPrefs.SetFloat(SFX_VOLUME_KEY, mSFXVolume);
+		PlayerPrefs.Save();
+	}
+
+	public void UpdateMusicVolume(float musicVolume)
+	{
+		mMusicVolume = musicVolume;
+//		if(AudioManager.pInstance)
+//			AudioManager.pInstance.UpdateMusicVolume(mMusicVolume);
+		PlayerPrefs.SetFloat(MUSIC_VOLUME_KEY, mMusicVolume);
+		PlayerPrefs.Save();
+	}
+
+	public void SetFirstTimeLauched(bool isFirstTimeLaunched)
+	{
+		mFirstTimeLaunch = isFirstTimeLaunched;
+		PlayerPrefs.SetInt(FIRST_TIME_LAUNCH, mFirstTimeLaunch? 1 : 0);
+		PlayerPrefs.Save();
+	}
+#endregion
+}
--- Assets/Scripts/GameElements/PlayerController.cs
+++ Assets/Scripts/GameElements/PlayerController.cs
@@ -1,16 +1,195 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public enum PlayerState
+{
+	SPAWN = 0,
+	IDLE,
+	RUNNING,
+	JUMP,
+	DEAD,
+	NONE = -1
+}
 
-	// Use this for initialization
-	void Start () {
-		
+public class PlayerController : MonoBehaviour
+{
+#region Variables
+	private const float JUMP_FORCE_MULITPLIER = 50.0f;
+	[SerializeField]
+	private float 		mJumpForce = 0;
+
+	private PlayerState mCurrentState;
+	private PlayerState mPrevState;
+	private Rigidbody2D	mRigidBody;
+	private PlayerData 	mPlayerData;
+	private Animator	mAnimator;
+	private float 		mStateTimer = 0;
+	private bool		mIsGrounded = true;
+	private bool 		mIsGameStarted;
+#endregion
+
+#region Properties
+	public PlayerState pCurrentState
+	{
+		get { return mCurrentState; }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public Rigidbody2D pRigidBody
+	{
+		get { return mRigidBody; }
 	}
+
+	public PlayerData pPlayerData
+	{
+		get { return mPlayerData; }
+	}
+#endregion
+
+#region Monobehaviour functions
+	private void Awake()
+	{
+		mRigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	private void Start()
+	{
+		mIsGrounded = false;
+		mCurrentState = PlayerState.SPAWN;
+		mPrevState = PlayerState.NONE;
+		mIsGameStarted = false;
+		mAnimator = GetComponentInChildren<Animator>();
+		if(mAnimator == null)
+			Debug.LogError("Cannot find Animator component");
+
+	}
+
+	private void OnEnable()
+	{
+		mCurrentState = PlayerState.SPAWN;
+		mPrevState = PlayerState.NONE;
+	}
+
+	private void OnDisable()
+	{
+	}
+
+	private void Update()
+	{
+		//Debug.Log("player id:"+m_PlayerIndex + " lifetime: " +m_Timer);
+		ProcessMovement();
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.tag == "Ground")
+			mIsGrounded = true;
+	}
+
+	private void OnCollisionStay2D(Collision2D other)
+	{
+		if(other.gameObject.tag == "Ground")
+			mIsGrounded = true;
+	}
+
+	private void OnCollisionExit2D(Collision2D other)
+	{
+		if(other.gameObject.tag == "Ground")
+			mIsGrounded = false;
+	}
+
+	private void OnDestroy()
+	{
+	}
+#endregion
+
+#region Class specific functions
+	public void Init(PlayerData playerData)
+	{
+		mPlayerData = playerData;
+	}
+
+	private void ProcessMovement()
+	{
+		if(mCurrentState != PlayerState.DEAD && mIsGrounded)
+		{
+			if(Input.GetMouseButtonDown(0))
+			{
+				if(mIsGameStarted)
+				{
+					SetState(PlayerState.JUMP);
+					mRigidBody.AddForce(Vector2.up * (mJumpForce * mRigidBody.mass * JUMP_FORCE_MULITPLIER));
+				}
+				SetState(PlayerState.RUNNING);
+			}
+		}
+
+		ProcessState();
+	}
+
+	public void SetState(PlayerState state)
+	{
+		if (mCurrentState == state)
+			return;
+
+		mPrevState = mCurrentState;
+		mCurrentState = state;
+
+		switch (mCurrentState)
+		{
+		case PlayerState.IDLE:
+			mRigidBody.velocity = Vector3.zero;
+			break;
+		case PlayerState.RUNNING:
+			break;
+		case PlayerState.JUMP:
+			mAnimator.SetBool("Run", false);
+			mAnimator.SetBool("Idle", false);
+			mAnimator.SetTrigger("Jump");
+			break;
+		case PlayerState.DEAD:
+			mRigidBody.velocity = Vector3.zero;
+			mAnimator.SetBool("Run", false);
+			mAnimator.SetTrigger("Fall");
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void ProcessState()
+	{
+		mStateTimer = Mathf.Max(0, mStateTimer - Time.deltaTime);
+
+		switch (mCurrentState)
+		{
+		case PlayerState.IDLE:
+			mAnimator.SetBool("Idle", true);
+			break;
+		case PlayerState.RUNNING:
+			mAnimator.SetBool("Run", true);
+			break;
+		case PlayerState.JUMP:
+			mAnimator.SetBool("Run", false);
+			mAnimator.SetBool("Idle", false);
+			break;
+		default:
+			break;
+		}
+	}
+
+	public bool CanProcessState(PlayerState state)
+	{
+		bool canProcess = false;
+		switch (state)
+		{
+		case PlayerState.RUNNING:
+			canProcess = (mCurrentState == PlayerState.IDLE && mCurrentState != PlayerState.DEAD && mCurrentState != PlayerState.JUMP);
+			break;
+		}
+
+		return canProcess;
+	}
+#endregion
 }
